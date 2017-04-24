dab2e0aac144c85907ac23c58fbb7dc652960b26
Pierre Genthon <genthon.pierre@gmail.com>
2017-04-24 21:48:50 +0200

    Adds basic user management, init capistrano

---
+++ app/mailers/admin_mailer.rb
@@ -0,0 +1,7 @@
+class AdminMailer < ApplicationMailer
+  def new_user_waiting_for_approval(user)
+    @user = user
+    admin = User.find()
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get "/signin" => "users/sessions#new", as: "new_user_session"
+    post "/signin" => "users/sessions#create", as: "user_session"
+    get "/signout" => "users/sessions#destroy", as: "destroy_user_session"
+    get "/signup" => "users/registrations#new", as: "new_user_registration"
+  end
+  resources :users, only: [:index, :show]
+  devise_for :users, skip: [:sessions], controllers: {registrations: 'users/registrations', passwords: 'users/passwords'}
+  root 'welcome#index'
 end
