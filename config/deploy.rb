8fe8ab253c325af89f97d5c384760e60aeda731a
Pierre Genthon <genthon.pierre@gmail.com>
2017-04-27 15:36:34 +0200

    deployment env setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, "git@github.com:ComicScrip/Projet-IPW.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/pierre-genthon.duckdns.org/Apps/assessme"
@@ -21,7 +22,7 @@ set :deploy_to, "/var/www/pierre-genthon.duckdns.org/Apps/assessme"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/local_env.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
