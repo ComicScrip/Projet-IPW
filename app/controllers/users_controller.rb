fdf4b0c8b0cf63c11b7ebd42840a8ebbb266a483
Pierre Genthon <genthon.pierre@gmail.com>
2017-04-30 13:39:59 +0200

    Adds possibility to add and remove roles for a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,15 +20,28 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+
+    Role.all.each {|role| add_or_remove_role(@user, role.name) }
+
     if @user.update(user_params)
       flash[:notice] = "L'utilisateur a bien été mis à jour."
     end
-    render 'edit'
+
+    redirect_to users_path
   end
 
   private
     def user_params
-      params.require(:user).permit(:firstName, :lastName, :email, :approved)
+      params.require(:user).permit(:firstName, :lastName, :email, :approved, :userRoles)
     end
 
+    def add_or_remove_role(user, role)
+      if params[:userRoles][role] == "1"
+        user.add_role role
+      elsif params[:userRoles][role] == "0"
+        user.remove_role role
+        # Rolify destroys the role in db if it's not referenced by any user, witch is not what we want...
+        Role.find_or_create_by(name: role)
+      end
+    end
 end
