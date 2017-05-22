7590aafa7febe81b583444cd4c2e2eda00f5cdb4
Pierre Genthon <genthon.pierre@gmail.com>
2017-05-22 11:16:06 +0200

    student gets email when graded, fix favicon

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,11 @@ class UserMailer < ApplicationMailer
     @user = user
     mail(to: @user.email, subject: 'Assessme - Votre compte vient d\'être activé !')
   end
+
+  def student_has_grade(student, exam)
+    @student = student
+    @exam = exam
+    mail(to: student.email, subject: 'Assessme - vous avez été noté pour l\'examen ' + exam.title + ' (' + exam.discipline.name + ')')
+  end
+
 end
--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -3,4 +3,12 @@ class Assessment < ApplicationRecord
   belongs_to :exam
 
   validates :grade, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 20}, allow_nil: true
+
+  after_update :mail_student_when_graded
+
+  def mail_student_when_graded
+    if self.grade != self.grade_was
+      UserMailer.student_has_grade(self.student, self.exam).deliver
+    end
+  end
 end
